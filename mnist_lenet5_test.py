517f3cd88464f31bb1f73e5e5a893e51025cd822
zh98530 <39448526+zh98530@users.noreply.github.com>
2018-12-12 12:50:11 +0800

    Create mnist_lenet5_test.py

---
+++ mnist_lenet5_test.py
@@ -0,0 +1,62 @@
+#coding:utf-8
+
+import time
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist_lenet5_forward
+import mnist_lenet5_backward
+import numpy as np
+
+TEST_INTERVAL_SECS = 5
+
+def test(mnist):
+	#复现训练完成的神经网络计算图
+	with tf.Graph().as_default() as g:
+		x = tf.placeholder(tf.float32,[
+			mnist.test.num_examples,
+			mnist_lenet5_forward.IMAGE_SIZE,
+			mnist_lenet5_forward.IMAGE_SIZE,
+			mnist_lenet5_forward.NUM_CHANNELS])
+		y_ = tf.placeholder(tf.float32,[None,mnist_lenet5_forward.OUTPUT_NODE])
+		y = mnist_lenet5_forward.forward(x,False,None)
+
+		#实例化saver对象，实现参数滑动平均值的加载
+		ema = tf.train.ExponentialMovingAverage(mnist_lenet5_forward)
+		ema_restore = ema.variables_to_restore()
+		saver = tf.train.Saver(ema_restore)
+		
+		#将BATCH_SIZE组数据，每一组数据取0-9中概率最大值，返回其标签，即0-9，与标准答案比对，相等返回true，不等返回false;将得到的布尔型数值转化为实数型，并将其取平均作为本组数据准确率
+		correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+		
+		while True:
+			#加载训练完成的模型
+			with tf.Session() as sess:
+				ckpt = tf.train.get_checkpoint_state(mnist_lenet5_backward.MODEL_SAVE_PATH)
+				if ckpt and ckpt.model_checkpoint_path:
+					#加载保存的会话
+					saver.restore(sess,ckpt.model_checkpoint_path)
+					
+					#提取轮数
+					global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+					reshaped_x = np.reshape(mnist.test.images,(
+									mnist.test.num_examples,
+									mnist_lenet5_forward.IMAGE_SIZE,
+									mnist_lenet5_forward.IMAGE_SIZE,
+									mnist_lenet5_forward.NUM_CHANNELS))
+					
+					#将训练集输入得数据准确率
+					accuracy_score = sess.run(accuracy,feed_dict={x:reshaped_x,y_:mnist.test.labels})
+					print("After %s training step(s), test accuracy = %g." %(global_step,accuracy_score))
+				else:
+					print('No checkpoints file found')
+					return
+			#定义每一轮进行5s延迟
+			time.sleep(TEST_INTERVAL_SECS)
+	
+def main():
+	mnist = input_data.read_data_sets("./data/",one_hot=True)
+	test(mnist)
+	
+if __name__ == '__main__':
+	main()
